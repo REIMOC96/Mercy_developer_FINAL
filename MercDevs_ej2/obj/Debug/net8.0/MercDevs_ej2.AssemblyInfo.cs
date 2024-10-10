6930f3aa91615813910d79cfe0c5dfb3e7dd3e52
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2024-10-10 04:49:50 -0300

    pre Fase 2

--- MercDevs_ej2/obj/Debug/net8.0/MercDevs_ej2.AssemblyInfo.cs
+++ MercDevs_ej2/obj/Debug/net8.0/MercDevs_ej2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MercDevs_ej2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46045bdbf8dde086a9782d61e764b99a4c0c964e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06cc6627227ffe452f24ee91d83fc2e6faa0202")]
 [assembly: System.Reflection.AssemblyProductAttribute("MercDevs_ej2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MercDevs_ej2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
